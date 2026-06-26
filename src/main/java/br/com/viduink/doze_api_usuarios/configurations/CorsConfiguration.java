a230ddc80a647d6e871e141b7addeca52c3c362a
Flipviduani <flipviduani@hotmail.com>
2026-06-26 13:02:27 -0300

    CORS setup, Docker MySQL service Health Check

--- src/main/java/br/com/viduink/doze_api_usuarios/configurations/CorsConfiguration.java
+++ src/main/java/br/com/viduink/doze_api_usuarios/configurations/CorsConfiguration.java
@@ -1,4 +1,26 @@
 package br.com.viduink.doze_api_usuarios.configurations;
 
-public class CorsConfiguration {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Value("${cors.frontend}")
+    private String corsFrontend;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        //Permissão para o projeto Angular
+        registry
+                .addMapping("/**") //Permissão para todos os ENDPOINTS
+                .allowedOrigins(corsFrontend) //Projeto Angular
+                .allowedMethods("POST", "PUT", "DELETE", "GET")
+                .allowedHeaders("*");
+    }
 }
